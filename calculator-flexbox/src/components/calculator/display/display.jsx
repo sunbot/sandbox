74c9a882b542f393e76f169427203771cb60f83e
Angela <angsunrey@gmail.com>
2019-06-30 21:59:27 -0500

    Add media query for calc size

--- calculator-flexbox/src/components/calculator/display/display.jsx
+++ calculator-flexbox/src/components/calculator/display/display.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import * as styles from "./display.module.css";
 
 const Display = () => {
-  return <div className={styles.display}>Screen Display</div>;
+  return (
+    <div className={styles.display}>
+      <p>0</p>
+    </div>
+  );
 };
 
 export default Display;
