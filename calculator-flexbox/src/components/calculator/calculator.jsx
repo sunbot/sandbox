857c3c4591acb7c57484ffbf28ce24298d1b5c36
Angela <angsunrey@gmail.com>
2019-07-04 16:52:52 -0500

    initial pass

--- calculator-flexbox/src/components/calculator/calculator.jsx
+++ calculator-flexbox/src/components/calculator/calculator.jsx
@@ -4,29 +4,67 @@ import Display from "./display";
 import styles from "./calculator.module.css";
 
 const Calculator = props => {
-  let buttonContents = [
+  const buttonContents = [
     ["ce", "c", "del", "/"],
     [7, 8, 9, "*"],
     [4, 5, 6, "-"],
     [1, 2, 3, "+"],
     ["+/-", 0, ".", "="]
   ];
-
-  const [result, setResult] = useState();
+  const [current, setCurrent] = useState("");
+  const [operator, setOperator] = useState("+");
+  const [leftOperand, setLeftOperand] = useState(0);
+  const [display, setDisplay] = useState("");
 
   const onHandleClick = input => {
-    if (result) setResult(result.toString() + input);
-    else {
-      setResult(input);
+    var isnum = /^\d+$/.test(input);
+    console.log("current " + current);
+
+    // if (input === "+/-") {
+    //   setCurrent(-current);
+    //   setDisplay(-current);
+    // } else
+    if (!isnum) {
+      const result = calculate(leftOperand, current, operator);
+      setOperator(input);
+      setCurrent("");
+      setLeftOperand(result);
+      setDisplay(result);
+    } else {
+      const result =
+        !display && input === 0 ? "" : current.toString() + input.toString();
+      setCurrent(result);
+      setDisplay(result);
     }
   };
+
+  function calculate(left, right, operator) {
+    const leftNum = Number(left);
+    const rightNum = Number(right);
+    if (operator === "+") {
+      const result = leftNum + rightNum;
+      console.log(`${left}${operator}${right}=${result}`);
+      console.log(result);
+      return result;
+    }
+    if (operator === "-") {
+      return leftNum - rightNum;
+    }
+    if (operator === "*") {
+      return leftNum * rightNum;
+    }
+    if (operator === "/") {
+      return leftNum / rightNum;
+    }
+  }
 
   let buttonRows = buttonContents.map((row, index) => {
     return <ButtonsRow key={index} buttons={row} onClick={onHandleClick} />;
   });
+  console.log("display:" + display);
   return (
     <div className={styles.container}>
-      <Display content={result} />
+      <Display content={display} />
       {buttonRows}
     </div>
   );
